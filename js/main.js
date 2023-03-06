38a2c939301f53c92d927ec378363f6e4fce6557
Daniela Mical <daniela.m.torrez14@gmail.com>
2023-03-06 13:10:42 -0400

    Primeiro commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 $('#telefone').mask('(00) 00000-0000')
 
+$('#cpf').mask('000-000-000-00')
+
+$('#cep').mask('00-000-000')
+
 $('form').validate({
     rules: {
         nome: {
@@ -24,6 +28,12 @@ $('form').validate({
     },
     messages: {
         nome: 'Por favor, insira o seu nome!'
+    },
+    messages: {
+        cpf: 'Por favor, insira o seu cpf certo!'
+    },
+    messages: {
+        cep: 'Por favor, insira o seu cep certo!'
     }
 
 })
