cfa5df33d50c638be26cef435cb79dccdc68d987
Daniela Mical <daniela.m.torrez14@gmail.com>
2023-03-06 15:44:24 -0400

    Primeiro commit

--- js/main.js
+++ js/main.js
@@ -5,10 +5,45 @@ $(document).ready(function(){
         var nome = $("#itnome").val;
         var email = $("#itemail").val;
         var telefone = $("#ittelefone").val;
+        var cpf = $("#itcpf").val;
+        var endereco = $("#itendereco").val;
+        var cep = $("#itcep").val;
 
         if(nome == ""){
             $("#mensagem1").fadeIn();
             return false;
+        }else{
+            $("#mensagem1").fadeOut();
         }
+        
+        if(email == ""){
+            $("#mensagem2").fadeIn();
+            return false;
+        }
+
+        
+        if(telefone == ""){
+            $("#mensagem3").fadeIn();
+            return false;
+        }
+
+        
+        if(cpf == ""){
+            $("#mensagem4").fadeIn();
+            return false;
+        }
+
+        
+        if(endereco == ""){
+            $("#mensagem5").fadeIn();
+            return false;
+        }
+
+        
+        if(cep == ""){
+            $("#mensagem6").fadeIn();
+            return false;
+        }
+
     });
 });
