dd71da54ecc8555e3dcea897f226c2eb95da1c51
Daniela Mical <daniela.m.torrez14@gmail.com>
2023-03-07 09:52:30 -0400

    Primeiro commit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,46 @@
+$("#formulario_contato").validate({
+    rules: {
+        nome: {
+            required: true,
+            minlength: 3,
+            maxlength: 30
+        },
+        email: {
+            required: true,
+            email: true
+        },
+        telefone: {
+            required: true,
+            required: true,
+            minlength: 1,
+            maxlength: 11
+        },
+        cpf: {
+            required: true,
+            minlength: 1,
+            maxlength: 11
+        },
+        endereco: {
+            required: true
+        },
+        cep: {
+            required: true,
+            minlength: 1,
+            maxlength: 8
+        }
+    }
+})
+
 $("#enviar").click(function(){
+    if($("#formulario_contato").valid() == false) {
+        return;
+    }
     let nome = $("#nome").val();
     let email = $("#email").val();
     let telefone = $("·telefone").val();
     let cpf = $("#cpf").val();
     let endereco = $("#endereco").val();
-    let cep = $("#cep").val();
+    let cep = $("#cep").val(); 
+
+    
 })
